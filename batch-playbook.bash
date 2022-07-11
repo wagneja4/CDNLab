53bbafa82997f44b95992f8e5beec680defcbbdb
wagneja4 <wagneja4@fel.cvut.cz>
2022-07-11 18:50:46 +0200

    wg playbooks

--- batch-playbook.bash
+++ batch-playbook.bash
@@ -3,4 +3,6 @@
 ansible-playbook ./playbooks/ansible.yml -i ./inventory/hosts --user ansible --ask-pass --ask-become-pass
 ansible-playbook ./playbooks/dns-master.yml -i ./inventory/hosts --user ansible --ask-pass --ask-become-pass
 ansible-playbook ./playbooks/wg.yml -i ./inventory/hosts --user ansible --ask-pass --ask-become-pass
-ansible-playbook ./playbooks/nginx.yml -i ./inventory/hosts --user ansible --ask-pass --ask-become-pass
+ansible-playbook ./playbooks/nginx-web.yml -i ./inventory/hosts --user ansible --ask-pass --ask-become-pass
+ansible-playbook ./playbooks/nginx-proxy.yml -i ./inventory/hosts --user ansible --ask-pass --ask-become-pass
+ansible-playbook ./playbooks/prometheus.yml -i ./inventory/hosts --user ansible --ask-pass --ask-become-pass
