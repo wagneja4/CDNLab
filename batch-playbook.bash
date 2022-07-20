dad93bdf0f4619560ad424030b839465d7b04c73
wagneja4 <wagneja4@fel.cvut.cz>
2022-07-20 17:11:52 +0200

    fixed dns resolving after reboot, fixed dns nginx crashes

--- batch-playbook.bash
+++ batch-playbook.bash
@@ -2,7 +2,7 @@
 # --ask-become-pass
 USER_NAME=user
 INVENTORY_PATH=./inventory/hosts
-WIREGUARD_IP=192.168.122.221
+WIREGUARD_IP=192.168.122.113
 for PLAYBOOK in dns-master.yml wg.yml nginx-web.yml nginx-proxy.yml prometheus.yml zookeeper.yml kafka.yml
 do
     ansible-playbook ./playbooks/$PLAYBOOK -i $INVENTORY_PATH --user $USER_NAME --ask-pass --ask-become-pass --extra-vars "root_dir=$(pwd) wireguardIP=$WIREGUARD_IP"
