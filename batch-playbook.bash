f70eb4973ca30173169da9149110e24ec30cd55c
wagneja4 <jenik.wagner@gmail.com>
2022-07-29 00:15:22 +0200

    forgot resolved task in some plays but resolved doesnt generate .../stub-resolv.conf hence it doesnt resolve til it does, added facts scripts

--- batch-playbook.bash
+++ batch-playbook.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 # --ask-become-pass
+ansible-galaxy collection install ansible.posix
 USER_NAME=user
 INVENTORY_PATH=./inventory/hosts
-WIREGUARD_IP=192.168.122.243
-for PLAYBOOK in dns-master.yml wg.yml nginx-web.yml nginx-proxy.yml prometheus.yml zookeeper.yml kafka.yml
+WIREGUARD_IP=192.168.122.253
+for PLAYBOOK in dns-master.yml wg.yml nginx-web.yml nginx-proxy.yml prometheus.yml zookeeper.yml # kafka.yml
 do
     ansible-playbook ./playbooks/$PLAYBOOK -i $INVENTORY_PATH --user $USER_NAME --ask-pass --ask-become-pass --extra-vars "root_dir=$(pwd) wireguardIP=$WIREGUARD_IP"
 done
