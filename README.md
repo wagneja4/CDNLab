74a2ee0e86fe49a22734a18b3ecf7eb36d18b76a
wagneja4 <jenik.wagner@gmail.com>
2022-07-29 20:42:48 +0200

    cleaned up

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CDNLab
 This is wagneja4's configuration of some popular services. The infrastructure is deployed with ansible. It is by no means perfect, I still have much to learn.
 
-Files which should be modified for further use:\
-./inventory/hosts ~ modify the ips of hosts.\
-batch-playbook.bash ~ relevant env. variables
+Files which should be modified before use:\
+./inventory/hosts ~ modify the ips of hosts and ip variables.\
+batch-playbook.bash ~ relevant env. variables, especially wireguard ip
+
--- batch-playbook.bash
+++ batch-playbook.bash
@@ -1,14 +1,18 @@
 #!/bin/bash
-
+#I use ansible.posix module instead of inlining/replacing existing config by myself. So I need this package
 ansible-galaxy collection install ansible.posix
 #
 USER_NAME=user
-INVENTORY_PATH=./inventory/hosts
+ROOT_DIRECTORY=$(pwd)
+INVENTORY_PATH=$ROOT_DIRECTORY/inventory/hosts
+# this should be replaced by domain name (in wg.conf files), but unfortunately wireguard doesnt support domain names (that I know of)
 WIREGUARD_IP=192.168.122.22
+# I should be using sshkeys. I ansible setup playbook (configuring ssh connection, disabling password authentication and etc.),
+# but I chose not to. This is a matter of system security, so I would abide by security policy in the organisation.
 ANSIBLE_PASSWORD=password
 BECOME_PASSWORD=password
 
-for PLAYBOOK in dns-master.yml wg.yml nginx-web.yml nginx-proxy.yml prometheus.yml zookeeper.yml # kafka.yml
+for PLAYBOOK in bind.yml wireguard.yml nginx-web.yml nginx-proxy.yml prometheus.yml zookeeper.yml
 do
-    ansible-playbook ./playbooks/$PLAYBOOK -i $INVENTORY_PATH --user $USER_NAME --extra-vars "root_dir=$(pwd) wireguardIP=$WIREGUARD_IP ansible_password=$ANSIBLE_PASSWORD ansible_become_password=$BECOME_PASSWORD" 
+    ansible-playbook ./playbooks/$PLAYBOOK -i $INVENTORY_PATH --user $USER_NAME --extra-vars "root_dir=$ROOT_DIRECTORY wireguardIP=$WIREGUARD_IP ansible_password=$ANSIBLE_PASSWORD ansible_become_password=$BECOME_PASSWORD" 
 done
