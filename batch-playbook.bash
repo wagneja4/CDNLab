01bb4a699fc8adf42b29cf50ad4d97a9fe43a968
wagneja4 <jenik.wagner@gmail.com>
2022-07-29 16:07:16 +0200

    offline journaling works, added reboot after play

--- batch-playbook.bash
+++ batch-playbook.bash
@@ -1,10 +1,14 @@
 #!/bin/bash
-# --ask-become-pass
+
 ansible-galaxy collection install ansible.posix
+
 USER_NAME=user
 INVENTORY_PATH=./inventory/hosts
-WIREGUARD_IP=192.168.122.253
+WIREGUARD_IP=192.168.122.239
+ANSIBLE_PASSWORD=password
+BECOME_PASSWORD=password
+
 for PLAYBOOK in dns-master.yml wg.yml nginx-web.yml nginx-proxy.yml prometheus.yml zookeeper.yml # kafka.yml
 do
-    ansible-playbook ./playbooks/$PLAYBOOK -i $INVENTORY_PATH --user $USER_NAME --ask-pass --ask-become-pass --extra-vars "root_dir=$(pwd) wireguardIP=$WIREGUARD_IP"
+    ansible-playbook ./playbooks/$PLAYBOOK -i $INVENTORY_PATH --user $USER_NAME --extra-vars "root_dir=$(pwd) wireguardIP=$WIREGUARD_IP ansible_password=$ANSIBLE_PASSWORD ansible_become_password=$BECOME_PASSWORD" 
 done
